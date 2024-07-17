82fc69462b4f4398618761ee30ed5b7dc38c344e
Jervani Z Thompson <jzthompson20@gmail.com>
2024-07-16 18:55:53 -0700

    implementing basic inverted_indexing

---
+++ src/inverted_index.hh
@@ -0,0 +1,20 @@
+#ifndef INVERTED_INDEX_HH
+#define INVERTED_INDEX_HH
+
+#include <unordered_map>
+#include <vector>
+#include <set>
+#include <string>
+
+class InvertedIndex {
+private:
+    std::unordered_map<std::string, std::set<int>> index;
+
+    std::set<int> intersect(const std::set<int>& set1, const std::set<int>& set2);
+
+public:
+    void addDocument(int docId, const std::string& content);
+    std::vector<int> search(const std::string& query);
+};
+
+#endif
